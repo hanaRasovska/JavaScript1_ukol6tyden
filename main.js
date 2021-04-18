8f85616856ee291798fb9d95f107f35c1fcd6a85
Hana Rašovská <hana.rasovska@gmail.com>
2021-04-18 19:45:15 +0200

    cyklus while - develop

--- main.js
+++ main.js
@@ -4,26 +4,27 @@
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
-let prvniPriklad=[];
-for ( let i = 0; i <= 10; i++){
+let i = 0;
+let prvniPriklad=[i];
+while (i < 10 ){
+  i++;
   prvniPriklad.push(i);
 }
-
 
 //TADY NAPIS CYKLUS
 
 console.log(prvniPriklad);
-
-
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
-let druhyPriklad=[];
-for (let i = 0; i <10; i++){
-  druhyPriklad.push(i);
+let j = 0;
+let druhyPriklad=[j];
+while (j <=10 ){
+  j++;
+  druhyPriklad.push(j);
 }
 
 console.log(druhyPriklad);
@@ -35,9 +36,12 @@ console.log(druhyPriklad.length);
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
+
 let tretiPriklad=[];
-for (i = 0; i <= 10; i++){
-  cislo = druhyPriklad.length - i;
+let k = 0;
+while  (k <= 10){
+  k++;
+  cislo = prvniPriklad.length - k;
   tretiPriklad.push(cislo);
 }
 
